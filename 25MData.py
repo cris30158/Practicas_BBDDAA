3bbdfcb998507c07f9131b07d7a73ced7f6ae011
ÁlvaroPC <alvaropc2362005@gmail.com>
2025-02-04 10:33:49 +0100

    python and sql code

--- 25MData.py
+++ 25MData.py
@@ -1,6 +1,30 @@
-def main():
-    return 0
+import random
+import csv
 
+def main():
+    #crear el archivo .csv
+    with open('PL1.csv', 'w', newline='') as file:
+        campos = ['id_producto', 'nombre', 'stock','precio']
+        writer = csv.DictWriter(file,fieldnames=campos)
+        writer.writeheader()
+
+        #rellenar los datos
+        for i in range (25000000):
+            #creamos los valores
+
+            id_producto = i+1
+            nombre = "producto_" + str(i+1)
+            stock = random.randint(0,20000)
+            precio = random.randint(0,5000)
+            writer.writerow({'id_producto': id_producto, 'nombre': nombre, 'stock':stock, 'precio': precio})
+            #los insertamos
+
+
+    
+
+
+
+    
 
 if __name__ == "__main__":  
     main()
---
+++ table25M.sql
@@ -0,0 +1,11 @@
+begin
+
+/c PL1 BBDDAA
+
+
+create table if not exists tabla25M(
+    id_producto int,
+    nombre text,
+    stock int,
+    precio int
+);
