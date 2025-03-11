0a1885c97e7f24cf6379370eadc58aeb11a6ccb0
ÁlvaroPC <alvaropc2362005@gmail.com>
2025-03-11 09:44:39 +0100

    mini changes

---
+++ PL2/codigo/main.sql
@@ -0,0 +1,20 @@
+\c investigar
+
+\set pager off
+set client_encoding ='UTF8';
+
+BEGIN;
+
+
+create table if not exists investigadores(
+    codigo_investigador numeric;
+    
+
+
+
+);
+
+
+
+
+COMMIT;
