c7aac3e2f6a91573b020c92c0e0a873e8700feb1
ÁlvaroPC <alvaropc2362005@gmail.com>
2025-03-11 09:59:47 +0100

    pregunta 2 a medias

--- PL2/codigo/main.sql
+++ PL2/codigo/main.sql
@@ -7,12 +7,38 @@ BEGIN;
 
 
 create table if not exists investigadores(
-    codigo_investigador numeric;
-    
-
-
-
+    codigo_investigador numeric,
+    nombre text,
+    apellido text,
+    salatio numeric,
+    PRIMARY KEY(codigo_investigador)
 );
+
+create table if not exists contratos(
+    codigo_contrato numeric,
+    nombre text,
+    entidad text,
+    coste numeric,
+    PRIMARY KEY(codigo_investigador)
+);
+
+create table if not exists investigadores_contratos(
+    codigo_investigador numeric, 
+    codigo_contrato numeric, 
+    horas numeric,
+    FOREIGN KEY (codigo_investigador) REFERENCES contratos(codigo_contrato)
+        ON DELETE RESTRICT
+        ON UPDATE RESTRICT,
+
+    FOREIGN KEY (codigo_contrato) REFERENCES contratos(codigo_contrato)
+        ON DELETE RESTRICT
+        ON UPDATE RESTRICT,
+
+    PRIMARY KEY (codigo_investigador,codigo_contrato)
+
+
+
+); 
 
 
 
