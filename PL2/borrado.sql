da8aca564c3bde803d9c1c1487454e6f3ee5880d
cris30158 <cristinamto08@gmail.com>
2025-04-20 21:36:31 +0200

    ajam

--- PL2/borrado.sql
+++ PL2/borrado.sql
@@ -1,51 +1,58 @@
-\c pl2
+
+
+
 BEGIN;
 
-CREATE TEMP TABLE IF NOT EXISTS temp_clientes AS
+
+CREATE INDEX IF NOT EXISTS idx_vehiculos_cleinteid_clientes ON vehiculos(clienteid_clientes);  
+CREATE INDEX IF NOT EXISTS idx_reservas_vehiculoid_vehiculos ON reservas(vehiculoid_vehiculos); 
+CREATE INDEX IF NOT EXISTS idx_reservas_plazaid_plazas ON reservas(plazaid_plazas); 
+CREATE INDEX IF NOT EXISTS idx_reservas_clienteid_clientes ON reservas(clienteid_clientes); 
+CREATE INDEX IF NOT EXISTS idx_incidencias_reservaid_reservas ON incidencias(reservaid_reservas);   
+CREATE INDEX IF NOT EXISTS idx_pagos_reservaid_reservas ON pagos(reservaid_reservas);   
+
+
+CREATE TEMP TABLE IF NOT EXISTS temp_clientes_a_borrar AS
     SELECT clienteid
     FROM clientes c 
     ORDER BY random()
     LIMIT (SELECT ceil(COUNT(*) * 0.3) FROM clientes);
 
-CREATE TEMP TABLE IF NOT EXISTS temp_vehiculos AS
+CREATE TEMP TABLE IF NOT EXISTS temp_vehiculos_a_borrar AS
 SELECT vehiculoid FROM vehiculos
-WHERE clienteid_clientes IN (SELECT clienteid FROM temp_clientes);
+WHERE clienteid_clientes IN (SELECT clienteid FROM temp_clientes_a_borrar);
 
-CREATE TEMP TABLE IF NOT EXISTS temp_reservas AS
+CREATE TEMP TABLE IF NOT EXISTS temp_reservas_a_borrar AS
 SELECT r.reservaid
 FROM reservas r
-WHERE r.clienteid_clientes IN (SELECT clienteid FROM temp_clientes)
-   OR r.vehiculoid_vehiculos IN (SELECT vehiculoid FROM temp_vehiculos);
+WHERE r.clienteid_clientes IN (SELECT clienteid FROM temp_clientes_a_borrar)
+   OR r.vehiculoid_vehiculos IN (SELECT vehiculoid FROM temp_vehiculos_a_borrar);
 
-CREATE INDEX IF NOT EXISTS idb_temp_vehiculos ON temp_vehiculos(vehiculoid);
-CREATE INDEX IF NOT EXISTS idb_temp_clientes ON temp_clientes(clienteid); 
-CREATE INDEX IF NOT EXISTS idb_temp_reservas ON temp_reservas(reservaid); 
 
-CREATE INDEX IF NOT EXISTS idb_vehiculos_con_cleinteid ON vehiculos(clienteid_clientes);  
-CREATE INDEX IF NOT EXISTS idb_reservas_con_vehiculoid ON reservas(vehiculoid_vehiculos); 
-CREATE INDEX IF NOT EXISTS idb_reservas_con_ ON reservas(plazaid_plazas); 
-CREATE INDEX IF NOT EXISTS idb_reservas_con_clienteid ON reservas(clienteid_clientes); 
-CREATE INDEX IF NOT EXISTS idb_incidencias_con_reservaid ON incidencias(reservaid_reservas);   
-CREATE INDEX IF NOT EXISTS idb_pagos_con_reservaid ON pagos(reservaid_reservas);   
+CREATE INDEX IF NOT EXISTS idx_temp_vehiculos_a_borrar ON temp_vehiculos_a_borrar(vehiculoid);
+CREATE INDEX IF NOT EXISTS idx_temp_clientes_a_borrar ON temp_clientes_a_borrar(clienteid); 
+CREATE INDEX IF NOT EXISTS idx_temp_reservas_a_borrar ON temp_reservas_a_borrar(reservaid); 
+
 
 DELETE FROM pagos p
-USING temp_reservas trb
+USING temp_reservas_a_borrar trb
 WHERE p.reservaid_reservas = trb.reservaid;
 
 DELETE FROM incidencias i
-USING temp_reservas trb
+USING temp_reservas_a_borrar trb
 WHERE i.reservaid_reservas = trb.reservaid;
 
 DELETE FROM reservas r
-USING temp_reservas trb
+USING temp_reservas_a_borrar trb
 WHERE r.reservaid = trb.reservaid;
 
 DELETE FROM vehiculos v
-USING temp_vehiculos tvb
+USING temp_vehiculos_a_borrar tvb
 WHERE v.vehiculoid = tvb.vehiculoid;
 
 DELETE FROM clientes c
-USING temp_clientes tcb
+USING temp_clientes_a_borrar tcb
 WHERE c.clienteid = tcb.clienteid;
+
 
 COMMIT;
--- PL2/datapark.sql
+++ PL2/datapark.sql
@@ -1,26 +1,24 @@
--- Database generated with pgModeler (PostgreSQL Database Modeler).
--- pgModeler version: 0.9.4
--- PostgreSQL version: 13.0
--- Project Site: pgmodeler.io
--- Model Author: ---
 
--- Database creation must be performed outside a multi lined SQL file. 
--- These commands were put in this file only as a convenience.
--- 
--- object: "PL2" | type: DATABASE --
--- DROP DATABASE IF EXISTS "PL2";
-CREATE DATABASE "DataPark"
-	ENCODING = 'UTF8'
-	LC_COLLATE = 'Spanish_Spain.1252'
-	LC_CTYPE = 'Spanish_Spain.1252'
-	TABLESPACE = pg_default
-	OWNER = postgres;
--- ddl-end --
+SET client_encoding = 'UTF8';
 
+BEGIN;
 
--- object: public.vehiculos | type: TABLE --
--- DROP TABLE IF EXISTS public.vehiculos CASCADE;
-CREATE TABLE public.vehiculos (
+-- Crear tabla de clientes
+CREATE TABLE clientes (
+	clienteid integer NOT NULL,
+	nombre text NOT NULL,
+	apellido text NOT NULL,
+	email text NOT NULL,
+	telefono integer NOT NULL, 
+	provincia text NOT NULL,
+	CONSTRAINT clientes_pkey PRIMARY KEY (clienteid),
+	CONSTRAINT email_unique UNIQUE (email),
+	CONSTRAINT clientes_telefono_key UNIQUE (telefono)
+	
+);
+
+-- Crear tabla de vehículos (depende de clientes)
+CREATE TABLE vehiculos (
 	vehiculoid integer NOT NULL,
 	matricula text NOT NULL,
 	marca text NOT NULL,
@@ -28,137 +26,73 @@ CREATE TABLE public.vehiculos (
 	color text NOT NULL,
 	clienteid_clientes integer,
 	CONSTRAINT vehiculos_pkey PRIMARY KEY (vehiculoid),
-	CONSTRAINT vehiculos_matricula_key UNIQUE (matricula)
+	CONSTRAINT vehiculos_matricula_key UNIQUE (matricula),
+    CONSTRAINT clientes_fk FOREIGN KEY (clienteid_clientes) REFERENCES clientes (clienteid) MATCH FULL ON DELETE RESTRICT ON UPDATE RESTRICT
 );
--- ddl-end --
-ALTER TABLE public.vehiculos OWNER TO postgres;
--- ddl-end --
 
--- object: public.clientes | type: TABLE --
--- DROP TABLE IF EXISTS public.clientes CASCADE;
-CREATE TABLE public.clientes (
-	clienteid integer NOT NULL,
-	nombre text NOT NULL,
-	apellido text NOT NULL,
-	email text NOT NULL,
-	telefono integer NOT NULL,
-	provincia text NOT NULL,
-	CONSTRAINT clientes_pkey PRIMARY KEY (clienteid),
-	CONSTRAINT clientes_telefono_key UNIQUE (telefono),
-	CONSTRAINT email_unique UNIQUE (email)
-);
--- ddl-end --
-ALTER TABLE public.clientes OWNER TO postgres;
--- ddl-end --
-
--- object: public.plazas | type: TABLE --
--- DROP TABLE IF EXISTS public.plazas CASCADE;
-CREATE TABLE public.plazas (
+-- Crear tabla de plazas (independiente)
+CREATE TABLE plazas (
 	plazaid integer NOT NULL,
 	numero integer NOT NULL,
 	nivel integer NOT NULL,
 	seccion text NOT NULL,
 	CONSTRAINT plazas_pkey PRIMARY KEY (plazaid)
 );
--- ddl-end --
-ALTER TABLE public.plazas OWNER TO postgres;
--- ddl-end --
 
--- object: public.reservas | type: TABLE --
--- DROP TABLE IF EXISTS public.reservas CASCADE;
-CREATE TABLE public.reservas (
+-- Crear tabla de reservas (depende de vehiculos, plazas y clientes)
+CREATE TABLE reservas (
 	reservaid integer NOT NULL,
 	fechainicio timestamp NOT NULL,
 	fechafin timestamp NOT NULL,
 	vehiculoid_vehiculos integer,
 	plazaid_plazas integer,
 	clienteid_clientes integer,
-	CONSTRAINT reservas_pkey PRIMARY KEY (reservaid)
-);
--- ddl-end --
-ALTER TABLE public.reservas OWNER TO postgres;
--- ddl-end --
 
--- object: public.pagos | type: TABLE --
--- DROP TABLE IF EXISTS public.pagos CASCADE;
-CREATE TABLE public.pagos (
+	CONSTRAINT reservas_pkey PRIMARY KEY (reservaid),
+    CONSTRAINT vehiculos_fk FOREIGN KEY (vehiculoid_vehiculos) REFERENCES vehiculos (vehiculoid) MATCH FULL ON DELETE RESTRICT ON UPDATE RESTRICT,   
+    CONSTRAINT clientes_fk FOREIGN KEY (clienteid_clientes) REFERENCES clientes (clienteid) MATCH FULL ON DELETE RESTRICT ON UPDATE RESTRICT,
+    CONSTRAINT plazas_fk FOREIGN KEY (plazaid_plazas) REFERENCES plazas (plazaid) MATCH FULL ON DELETE RESTRICT ON UPDATE RESTRICT
+);
+
+-- Crear tabla de pagos (depende de reservas)
+CREATE TABLE pagos (
 	pagoid integer NOT NULL,
 	cantidad numeric(10,2) NOT NULL,
 	fechapago timestamp NOT NULL,
 	metodopago text NOT NULL,
 	reservaid_reservas integer,
-	CONSTRAINT pagos_pkey PRIMARY KEY (pagoid)
+	CONSTRAINT pagos_pkey PRIMARY KEY (pagoid),
+    CONSTRAINT reservas_fk FOREIGN KEY (reservaid_reservas) REFERENCES reservas (reservaid) MATCH FULL ON DELETE RESTRICT ON UPDATE RESTRICT
 );
--- ddl-end --
-ALTER TABLE public.pagos OWNER TO postgres;
--- ddl-end --
 
--- object: public.incidencias | type: TABLE --
--- DROP TABLE IF EXISTS public.incidencias CASCADE;
-CREATE TABLE public.incidencias (
+-- Crear tabla de incidencias (depende de reservas)
+CREATE TABLE incidencias (
 	incidenciaid integer NOT NULL,
 	descripcion text NOT NULL,
 	fechaincidencia timestamp NOT NULL,
 	estado text NOT NULL,
 	reservaid_reservas integer,
-	CONSTRAINT incidencias_pkey PRIMARY KEY (incidenciaid)
+	CONSTRAINT incidencias_pkey PRIMARY KEY (incidenciaid),
+    CONSTRAINT reservas_fk FOREIGN KEY (reservaid_reservas) REFERENCES reservas (reservaid) MATCH FULL ON DELETE RESTRICT ON UPDATE RESTRICT
 );
--- ddl-end --
-ALTER TABLE public.incidencias OWNER TO postgres;
--- ddl-end --
-
--- object: vehiculos_fk | type: CONSTRAINT --
--- ALTER TABLE public.reservas DROP CONSTRAINT IF EXISTS vehiculos_fk CASCADE;
-ALTER TABLE public.reservas ADD CONSTRAINT vehiculos_fk FOREIGN KEY (vehiculoid_vehiculos)
-REFERENCES public.vehiculos (vehiculoid) MATCH FULL
-ON DELETE RESTRICT ON UPDATE RESTRICT;
--- ddl-end --
-
--- object: reservas_fk | type: CONSTRAINT --
--- ALTER TABLE public.incidencias DROP CONSTRAINT IF EXISTS reservas_fk CASCADE;
-ALTER TABLE public.incidencias ADD CONSTRAINT reservas_fk FOREIGN KEY (reservaid_reservas)
-REFERENCES public.reservas (reservaid) MATCH FULL
-ON DELETE RESTRICT ON UPDATE RESTRICT;
--- ddl-end --
-
--- object: clientes_fk | type: CONSTRAINT --
--- ALTER TABLE public.vehiculos DROP CONSTRAINT IF EXISTS clientes_fk CASCADE;
-ALTER TABLE public.vehiculos ADD CONSTRAINT clientes_fk FOREIGN KEY (clienteid_clientes)
-REFERENCES public.clientes (clienteid) MATCH FULL
-ON DELETE RESTRICT ON UPDATE RESTRICT;
--- ddl-end --
-
--- object: reservas_fk | type: CONSTRAINT --
--- ALTER TABLE public.pagos DROP CONSTRAINT IF EXISTS reservas_fk CASCADE;
-ALTER TABLE public.pagos ADD CONSTRAINT reservas_fk FOREIGN KEY (reservaid_reservas)
-REFERENCES public.reservas (reservaid) MATCH FULL
-ON DELETE RESTRICT ON UPDATE RESTRICT;
--- ddl-end --
-
--- object: plazas_fk | type: CONSTRAINT --
--- ALTER TABLE public.reservas DROP CONSTRAINT IF EXISTS plazas_fk CASCADE;
-ALTER TABLE public.reservas ADD CONSTRAINT plazas_fk FOREIGN KEY (plazaid_plazas)
-REFERENCES public.plazas (plazaid) MATCH FULL
-ON DELETE RESTRICT ON UPDATE RESTRICT;
--- ddl-end --
-
--- object: clientes_fk | type: CONSTRAINT --
--- ALTER TABLE public.reservas DROP CONSTRAINT IF EXISTS clientes_fk CASCADE;
-ALTER TABLE public.reservas ADD CONSTRAINT clientes_fk FOREIGN KEY (clienteid_clientes)
-REFERENCES public.clientes (clienteid) MATCH FULL
-ON DELETE RESTRICT ON UPDATE RESTRICT;
--- ddl-end --
-
--- object: "grant_CU_eb94f049ac" | type: PERMISSION --
-GRANT CREATE,USAGE
-   ON SCHEMA public
-   TO postgres;
--- ddl-end --
-
--- object: "grant_CU_cd8e46e7b6" | type: PERMISSION --
-GRANT CREATE,USAGE
-   ON SCHEMA public
-   TO PUBLIC;
--- ddl-end --
 
 
+COMMIT;
+
+/* 
+\i datapark.sql
+ \timing
+
+\copy clientes(clienteid, nombre, apellido, email, telefono, provincia) FROM 'clientes.csv' DELIMITER ',' CSV HEADER NULL 'NULL';
+
+\copy vehiculos(vehiculoid, matricula, marca, modelo, color, clienteid_clientes) FROM 'vehiculos.csv' DELIMITER ',' CSV HEADER NULL 'NULL';
+
+\copy plazas(plazaid, numero, seccion, nivel) FROM 'plazas.csv' DELIMITER ',' CSV HEADER NULL 'NULL';
+
+\copy reservas(reservaid, vehiculoid_vehiculos, plazaid_plazas, clienteid_clientes, fechainicio, fechafin) FROM 'reservas_final.csv' DELIMITER ',' CSV HEADER NULL 'NULL';
+
+\copy pagos(pagoid, cantidad, fechapago, metodopago, reservaid_reservas) FROM 'pagos.csv' DELIMITER ',' CSV HEADER NULL 'NULL';
+
+\copy incidencias(incidenciaid, reservaid_reservas, estado, fechaincidencia, descripcion)  FROM 'incidencias.csv' DELIMITER ',' CSV HEADER NULL 'NULL';
+
+*/
